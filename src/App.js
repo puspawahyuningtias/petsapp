d3e8d2c9e9304040754f59cea282359e080a9e9d
Puspa Wahyuningtias <puspa.wahyuningtias@gmail.com>
2021-12-17 15:15:24 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Categories from "./components/categories";
+import Menu from "./components/menu";
+import menu from "./components/data";
+import "./components/style.css";
+
+const allCategories = ["all", ...new Set(menu.map((item) => item.category))];
 
 function App() {
+  const [menuItems, setMenuItems] = useState(menu);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(menu);
+      return;
+    }
+    const newItems = menu.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
   return (
     <div className="App">
+      <div className="title">
+        <h1>PETS</h1>
+      </div>
+      <Categories categories={categories} filterItems={filterItems} />
+      <Menu menuItems={menuItems} />
     </div>
   );
 }
