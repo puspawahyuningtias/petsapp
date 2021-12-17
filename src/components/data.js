6dec58160ced2444f7558439ca722e8375a29944
Puspa Wahyuningtias <puspa.wahyuningtias@gmail.com>
2021-12-17 15:14:52 +0700

    Create data.js

---
+++ src/components/data.js
@@ -0,0 +1,73 @@
+const menu = [
+  {
+    id: 1,
+    breed: "anggora",
+    category: "cat",
+    img: "https://cdn.medcom.id/images/library/images/bola/hilman/kucing%20angora.jpg",
+    desc: "Anggora adalah jenis ras kucing yang paling populer di Indonesia karena bulunya yang super lebat dan panjang sehingga terlihat anggun. Kucing ini berasal dari Turki.",
+  },
+  {
+    id: 2,
+    breed: "persia",
+    category: "cat",
+    img: "https://cdn.medcom.id/images/library/images/bola/hilman/kucing%20persia.jpg",
+    desc: "Persia adalah jenis kucing yang memiliki bulu lebat dan bentuk kepala kucing persia cenderung bulat, sehingga membuat wajahnya terlihat menggemaskan.",
+  },
+  {
+    id: 3,
+    breed: "siam",
+    category: "cat",
+    img: "https://cdn.medcom.id/images/library/images/bola/hilman/kucing%20siam.jpg",
+    desc: "Siam adalah jenis kucing yang berasal dari Siam atau Thailand. Dibanding ras lain, kucing Siam dianggap paling cerdas karena di negara asalnya terbiasa dilatih untuk bermain di dalam sirkus.",
+  },
+  {
+    id: 4,
+    breed: "sphynx",
+    category: "cat",
+    img: "https://cdn.medcom.id/images/library/images/bola/hilman/kucing%20sphynx.jpg",
+    desc: "Sphynx adalah jenis kucing tak berbulu yang berhasil dikembangkan oleh Georgiana Gatenby dari Minnesota. Kucing ini bernilai mencapai puluhan juta Rupiah",
+  },
+  {
+    id: 5,
+    breed: "Scottish Fold",
+    category: "cat",
+    img: "https://cdn.medcom.id/images/library/images/bola/hilman/Kucing%20Scottish%20Fold.jpg",
+    desc: "Scottish Fold adalah kucing ras yang berasal dari Skotlandia. Kucing ini sangat unik karena bentuk ujung telinganya yang seperti terlipat sehingga terlihat menggemaskan.",
+  },
+  {
+    id: 6,
+    breed: "Tibetan Mastiff",
+    category: "dog",
+    img: "https://asset.kompas.com/crops/zPf9ShkWMC4F_kPdtYgBR4l8sko=/77x0:921x563/750x500/data/photo/2018/05/16/3496609598.jpg",
+    desc: "Tibetan Mastiff adalah ras anjing terkuat. Sehingga, sering digunakan untuk menjaga rumah karena fisiknya yang kuat dan gigitannya yang dapat memberikan tekanan seberat 249 kg.",
+  },
+  {
+    id: 7,
+    breed: "Saint Bernard",
+    category: "dog",
+    img: "https://asset.kompas.com/crops/msP1i1iYp8U0JPFCG3p5M9AvL4s=/192x128:1728x1152/750x500/data/photo/2021/07/12/60eb7dd114072.jpg",
+    desc: 'Saint Bernard atau sering disebut "gentle giant", karena sering tak menyadari kekuatannya sendiri. Sehingga, pemiliknya dianjurkan dilatih untuk mencegahnya tak sengaja membuat orang pingsan.',
+  },
+  {
+    id: 8,
+    breed: "kangal",
+    category: "dog",
+    img: "https://asset.kompas.com/crops/O-xpD63dDHRbWp5td_aQyaUTFCk=/18x25:982x667/750x500/data/photo/2021/10/22/61722c42a600f.jpg",
+    desc: "Kangal merupakan anjing kuat lainnya yang berasal dari Turki. Dengan gigitan yang kuat biasanya kangal dijadikan penjaga rumah.",
+  },
+  {
+    id: 9,
+    breed: "Rottweiler",
+    category: "dog",
+    img: "https://asset.kompas.com/crops/oergwytrd6s5MhNy8tZN0cu0h9w=/0x0:1920x1280/750x500/data/photo/2021/02/15/602a97981d935.jpg",
+    desc: "Rottweiler dikenal sebagai anjing yang agresif. Padahal Rottweiler yang terlatih dapat menjadi hewan peliharaan keluarga yang lembut dan setia.",
+  },
+  {
+    id: 10,
+    breed: "siberian husky",
+    category: "dog",
+    img: "https://asset.kompas.com/crops/vaS6YwegxfpB1zlNorcq_qisgbg=/0x0:750x500/750x500/data/photo/2021/05/29/60b1eae36a9eb.jpg",
+    desc: "Siberian Husky memiliki Kekuatan dan stamina  yan luar biasa. Terbukti dengan pekerjaannya sehsari-hari, menarik kereta luncur yang penuh dengan berbagai muatan, melewati hutan belantara Arktik yang beku.",
+  },
+];
+export default menu;
